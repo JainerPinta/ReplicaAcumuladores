d8ca577b4a20ddd42e3874f226f3b98caa59bab0
JainerPinta <95033872+JainerPinta@users.noreply.github.com>
2022-02-18 20:53:42 -0500

    Add files via upload

---
+++ Acumuladores/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/ise/Desktop/Proyectos/Acumuladores/TB_Acumulador_A_isim_beh.exe" -prj "/home/ise/Desktop/Proyectos/Acumuladores/TB_Acumulador_A_beh.prj" "work.TB_Acumulador_A" 
